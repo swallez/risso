58afe578cb53ac7c1b960f30f5903cad5b8d7aaa
Sylvain Wallez <sylvain@bluxte.net>
2018-12-22 15:06:58 +0100

    Remove risso_actix::log_macros module that is now in risso_api

--- risso_actix/src/main.rs
+++ risso_actix/src/main.rs
@@ -16,13 +16,6 @@ use futures::prelude::*;
 
 use crate::logs::RequestLogger;
 
-mod log_macros {
-    // Since slog also defines log's macros, we can't blindly import "slog::*" but always repeating
-    // these imports is a pain. So just `use crate::log_macros::*` and you're all set.
-    pub use log::{debug, error, info, trace, warn};
-    pub use slog::{slog_crit, slog_debug, slog_error, slog_info, slog_log, slog_o, slog_trace, slog_warn};
-}
-
 fn unsubscribe(state: State<ApiContext>, path: Path<(String, String, String)>) -> impl Responder {
     let (id, email, key) = path.into_inner();
 
